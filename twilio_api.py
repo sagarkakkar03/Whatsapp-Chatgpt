45d588859f79e2353e20d7698ce4b0b6741db9f9
sagarkakkar03 <122864251+sagarkakkar03@users.noreply.github.com>
2023-10-03 07:51:09 +0530

    Update twilio_api.py

--- twilio_api.py
+++ twilio_api.py
@@ -1,7 +1,7 @@
 import os
 from twilio.rest import Client
-account_sid = "AC5d92e3f3196bc6c9fbe9472d496fb474"
-auth_token = "db29f68c2d5077bbf09495f055e5e7cc"
+# account_sid = 
+# auth_token = 
 client = Client(account_sid, auth_token)
 def send_messsage(to:str, message:str):
     client.messages.create(
